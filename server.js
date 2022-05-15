f3da4ff6533842d2ed401a65721f9851c978a0ac
Federico Baldini <federico.baldini7@gmail.com>
2022-05-15 11:10:42 +0200

    Update server.js
    Now .wasm files are served with the correct mime type.

--- server.js
+++ server.js
@@ -3,7 +3,16 @@ import express from "express";
 const server = express();
 
 // Serve static files from /public
-server.use(express.static("public"));
+server.use(
+  express.static("public", {
+    setHeaders: (res, path, stat) => {
+      // Serve .wasm files with correct mime type
+      if (path.endsWith(".wasm")) {
+        res.set("Content-type", "application/wasm");
+      }
+    },
+  })
+);
 
 // Start server
 server.listen(2222, () => console.log("Server running on port 2222!"));

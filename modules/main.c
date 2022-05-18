00fdbcff0aa99e3eea24964fce2dfc44e362b50e
Federico Baldini <federico.baldini7@gmail.com>
2022-05-18 08:06:27 +0200

    Update main.c
    Calling JavaScript functions from C.

--- modules/main.c
+++ modules/main.c
@@ -1,10 +1,19 @@
 // WasmFiddle converter from C to Wasm: https://wasdk.github.io/WasmFiddle/
+
+void num_log(int number);
+void str_log(char *message);
+
 int main()
 {
   return 42;
 }
 
-int get_double_number(int number)
+void greet()
 {
-  return number * 2;
+  str_log("Hello from C programming language!");
+}
+
+void get_double_number(int number)
+{
+  num_log(number * 2);
 }

c69ff3ca61b9405f79d8772657775166ff6c1b50
Federico Baldini <federico.baldini7@gmail.com>
2022-05-21 12:00:33 +0200

    Update main.js
    Created the correct function to read a string from memory.

--- public/main.js
+++ public/main.js
@@ -1,8 +1,30 @@
+// Read a string out of Web Assembly memory
+const readStringFromMemory = (offset, length) => {
+  const stringBuffer = new Uint8Array(
+    wasm.instance.exports.memory.buffer,
+    offset,
+    length
+  );
+  const decodedString = new TextDecoder().decode(stringBuffer);
+  // Notify and make use of new string
+  window.dispatchEvent(
+    new CustomEvent("read-string-from-memory", { detail: decodedString })
+  );
+};
+
+// Listen for new wasm strings
+window.addEventListener("read-string-from-memory", (readString) => {
+  console.log(
+    "Received a new string from C programming lenguage:",
+    readString.detail
+  );
+});
+
 // Imports object
 const imports = {
   env: {
     num_log: console.log,
-    str_log: console.log,
+    str_log: readStringFromMemory,
   },
 };
 
@@ -12,7 +34,7 @@ WebAssembly.instantiateStreaming(fetch("/program.wasm"), imports).then(
     console.log("WASM Ready.");
     // Make the wasm object accessible
     window.wasm = wasm;
-    
+
     // Log out all the exported functions
     console.log(WebAssembly.Module.exports(wasm.module));
     // Log out all the imported functions

4894fcf2d21a35c34ec9cfbdee3df77b2ede32a7
Federico Baldini <federico.baldini7@gmail.com>
2022-05-22 19:00:26 +0200

    Update main.js
    Created memory variable of two pages to refer for read data from memory.

--- public/main.js
+++ public/main.js
@@ -1,10 +1,10 @@
+// Initialise wasm with custom memory (array buffer)
+// 2 pages: 2 * 64kB (128kB)
+const wasmMemory = new WebAssembly.Memory({ initial: 2 });
+
 // Read a string out of Web Assembly memory
 const readStringFromMemory = (offset, length) => {
-  const stringBuffer = new Uint8Array(
-    wasm.instance.exports.memory.buffer,
-    offset,
-    length
-  );
+  const stringBuffer = new Uint8Array(wasmMemory.buffer, offset, length);
   const decodedString = new TextDecoder().decode(stringBuffer);
   // Notify and make use of new string
   window.dispatchEvent(
@@ -25,11 +25,12 @@ const imports = {
   env: {
     num_log: console.log,
     str_log: readStringFromMemory,
+    memory: wasmMemory,
   },
 };
 
 // Load WASM
-WebAssembly.instantiateStreaming(fetch("/program.wasm"), imports).then(
+WebAssembly.instantiateStreaming(fetch("/main.wasm"), imports).then(
   (wasm) => {
     console.log("WASM Ready.");
     // Make the wasm object accessible

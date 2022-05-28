66e254daf83041155655860b70c534a38c7131f7
Federico Baldini <federico.baldini7@gmail.com>
2022-05-28 23:40:28 +0200

    Update main.c
    Created new functions to export to JavaScript.

--- libraries/main.c
+++ libraries/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
   printf("WASM Ready - Testing with Emscripten HTML!\n");
   return 42;
+}
+
+int get_number()
+{
+  return 22;
+}
+
+int get_double_number(int number)
+{
+  return number * 2;
+}
+
+char *greet(char *name)
+{
+  char greeting[50];
+  if (strlen(name) > 40)
+  {
+    return "Name too long";
+  }
+  strcpy(greeting, "Hello ");
+  return strcat(greeting, name);
 }

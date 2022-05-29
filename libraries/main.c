a333359f14131752ced68e79481b38bf919c2b52
Federico Baldini <federico.baldini7@gmail.com>
2022-05-29 12:27:07 +0200

    Update main.c
    Called and declared JavaScript functions from C.

--- libraries/main.c
+++ libraries/main.c
@@ -1,29 +1,30 @@
+#include <emscripten.h>
 #include <stdio.h>
-#include <string.h>
+
+// Declare a reusable JavaScript function
+EM_JS(void, javascript_function, (int number), {console.log("Call from EM_JS: " + number)});
 
 int main()
 {
-  printf("WASM Ready - Testing with Emscripten HTML!\n");
+  printf("WASM Ready\n");
+
+  // Call JavaScript function (eval)
+  emscripten_run_script("console.log('Hello from C!')");
+
+  // Call JavaScript function ASYNC (eval)
+  emscripten_async_run_script("console.log('Hello from C - ASYNC!')", 2000);
+
+  // Get return value from JavaScript function - INT
+  int javascript_value_int = emscripten_run_script_int("getNumber()");
+
+  // Get return value from JavaScript function - STRING
+  char *javascript_value_string = emscripten_run_script_string("getString()");
+
+  printf("Value from getNumber() function: %d\n", javascript_value_int);
+  printf("Value from getString() function: %s\n", javascript_value_string);
+
+  // Execute EM_JS function
+  javascript_function(144);
+
   return 42;
-}
-
-int get_number()
-{
-  return 22;
-}
-
-int get_double_number(int number)
-{
-  return number * 2;
-}
-
-char *greet(char *name)
-{
-  char greeting[50];
-  if (strlen(name) > 40)
-  {
-    return "Name too long";
-  }
-  strcpy(greeting, "Hello ");
-  return strcat(greeting, name);
 }
